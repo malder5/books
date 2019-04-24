eea96483fe77d91534abf695cf99391f851d6a69
Roman <mulyarchik.rk@yandex.ru>
2019-04-24 10:31:17 +0300

    Добавлено получение кода HTML

---
+++ get_books.py
@@ -0,0 +1,23 @@
+import requests
+
+
+
+def main():
+    url = 'https://ya.ru'
+    get_html(url)
+
+
+
+def get_html(url):
+    try:
+        user_agent = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+        }
+        r = requests.get(url=url, headers=user_agent)
+        print(r.text)
+    except:
+        print(r.status_code)
+
+
+if __name__ == '__main__':
+    main()
